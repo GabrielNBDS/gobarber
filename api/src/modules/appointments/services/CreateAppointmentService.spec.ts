2f1601d255d8fbc6f95a13305af68d37585f6c8b
GabrielNBDS <gabriel.nbds@gmail.com>
2020-05-13 11:56:59 -0400

    Api: Finishing tests

--- api/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ api/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -32,7 +32,7 @@ describe('CreateAppointment', () => {
       provider_id: '303030',
     });
 
-    expect(
+    await expect(
       createAppointment.execute({
         date: appointmentDate,
         provider_id: '303030',
