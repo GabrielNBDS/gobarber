3ae6a430cb22f9cc0f1eb0349753e128d4223ccd
GabrielNBDS <gabriel.nbds@gmail.com>
2020-05-20 14:04:52 -0400

    Api: Added mongodb

---
+++ api/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
+
+export default class AppointmentController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const provider_id = request.user.id;
+    const { day, month, year } = request.body;
+
+    const listProvidrAppointmens = container.resolve(
+      ListProviderAppointmentsService,
+    );
+
+    const appointments = await listProvidrAppointmens.execute({
+      provider_id,
+      day,
+      month,
+      year,
+    });
+
+    return response.json(appointments);
+  }
+}
---
+++ api/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -0,0 +1,39 @@
+import { injectable, inject } from 'tsyringe';
+
+import Appointments from '../infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  day: number;
+  month: number;
+  year: number;
+}
+
+@injectable()
+class ListProvidersMonthAvailabilityService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    day,
+    year,
+    month,
+  }: IRequest): Promise<Appointments[]> {
+    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+      {
+        day,
+        month,
+        provider_id,
+        year,
+      },
+    );
+
+    return appointments;
+  }
+}
+
+export default ListProvidersMonthAvailabilityService;
